1f10765038b53b7ee0ae54b8ab8af510b1eaaaff
ANTHONUS <anthonus.d@gmail.com>
2025-02-26 14:38:50 +0100

    site final, sans responsive
    background.mp4 n'a pas été envoyé dans le repo car c'est un gros fichier.

--- Assets/Scripts/peak.js
+++ Assets/Scripts/peak.js
@@ -1,12 +1,15 @@
 window.addEventListener("scroll", function() {
     let header = document.getElementById("header");
+    let footer = document.getElementById("footer");
     let accueilH1 = document.getElementById("accueil-h1");
     
     let targetRect = accueilH1.getBoundingClientRect();
 
     if (targetRect.bottom <= 0) {
-        header.style.transform = "translateY(120px)";
+        header.style.transform = "translateY(110px)";
+        footer.style.transform = "translateY(-50px)";
     } else {
         header.style.transform = "translateY(-100px)";
+        footer.style.transform = "translateY(50px)";
     }
 });
