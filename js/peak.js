b6e885b1b896d8a1c64d4608d8522cfd83ea7971
ANTHONUSS <anthonus.d@gmail.com>
2025-09-11 20:06:44 +0200

    modification du css pour plus de lisibilité

--- js/peak.js
+++ js/peak.js
@@ -2,14 +2,14 @@ window.addEventListener("scroll", function() {
     let header = document.getElementById("header");
     let footer = document.getElementById("footer");
     let accueilIMG = document.getElementById("accueil-img");
-    
-    let targetRect = accueilIMG.getBoundingClientRect();
-    let isAtBottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight-100;
 
-    if(isAtBottom) {
+    let targetRect = accueilIMG.getBoundingClientRect();
+    let isAtBottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100;
+
+    if (isAtBottom) {
         footer.style.transform = "translateY(-300px)";
-    }else {
-        if (targetRect.bottom - targetRect.height/2 <= 0) {
+    } else {
+        if (targetRect.bottom - targetRect.height / 2 <= 0) {
             header.style.transform = "translateY(110px)";
             footer.style.transform = "translateY(-50px)";
         } else {
