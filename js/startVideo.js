b7b6539448f1ee236ee50ea9d8a93d390313c92b
ANTHONUSS <anthonus.d@gmail.com>
2025-09-26 22:36:44 +0200

    ajout du autoplay dans le 404

---
+++ js/startVideo.js
@@ -0,0 +1,19 @@
+const video = document.getElementById('rickRoll');
+
+window.addEventListener('load', () => {
+    video.play().catch(err => {
+        console.log('Lecture automatique bloquée par le navigateur :', err);
+    });
+});
+
+window.addEventListener('touchstart', () => {
+    video.muted = false;
+    video.play();
+}, { once: true });
+
+function enableSound() {
+    video.muted = false;
+    video.play();
+    window.removeEventListener('click', enableSound);
+}
+window.addEventListener('click', enableSound);
